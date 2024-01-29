e5befef7c5aff88158778a39e60f2bf132fddc32
FnnyJsst <fanny.jousset@gmail.com>
2024-01-29 10:09:55 +0100

    added form

---
+++ cake-nest/src/components/Form.jsx
@@ -0,0 +1,11 @@
+function Form () {
+return (
+    <>
+    <form>
+        <input type="text" placeholder="Entrez votre prénom" /><button>Accédez à votre espace</button>
+    </form>
+    </>
+)
+}
+
+export default Form
